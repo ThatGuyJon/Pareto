ebde218a91de457bdac1a73de545f83fbd3cde10
ThatGuyJon <54686106+ThatGuyJon@users.noreply.github.com>
2024-04-03 13:05:55 -0400

    Update ParetoInstaller.py

--- ParetoInstaller.py
+++ ParetoInstaller.py
@@ -3,9 +3,10 @@ import base64
 
 # Create a dictionary of business names and their corresponding options
 business_options_dict = {
-    "Business A": {
-        "Workstation": "[1](https://example.com/businessA_workstation.exe)",
-        "Server": "[2](https://example.com/businessA_server.exe)"
+    "LavalTool": {
+        "Workstation Plant 1": "[1](https://api.paretosupport.com/clients/d8e47500-b39c-4e1f-b62b-d265f0af13ef/deploy/)",
+        "Workstation Plant 2": "[1](https://api.paretosupport.com/clients/f3c7d97a-d198-49a7-9138-57de0921dd96/deploy/)",
+        "Server": "[2](https://api.paretosupport.com/clients/42fcf95e-37b3-40ae-81b4-8d75fbcae718/deploy/)"
     },
     "Business B": {
         "Workstation": "[3](https://example.com/businessB_workstation.exe)",
@@ -24,7 +25,7 @@ selected_option = st.selectbox("Select an option", list(business_options_dict[se
 def create_download_link(url, filename):
     with st.spinner("Generating download link..."):
         # You can modify this part based on your file format (e.g., .exe)
-        href = f'<a href="{url}" download="{filename}">Click here to download</a>'
+        href = f'<a href="{url}" download="{filename}"><button>Click here to download</button></a>'
         return href
 
 # Get the selected URL based on the chosen business and option
